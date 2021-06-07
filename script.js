e56f54eabfd664b6bdea3351dd0df81d2a7eba5a
Антон <tohanknv@yandex.ru>
2021-06-07 18:54:34 +0300

    js done, no styles ready

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
+
+
 let toDoList = document.getElementById("tasks-wrapper"),
     btn = document.getElementById("button"),
     action = document.getElementById("whatToDo"),
-    wrapper = document.getElementById("wrapper"),
     deeds = [];
+    deedsItems = [];
 
 localStorage.deeds ? deeds = JSON.parse(localStorage.getItem('deeds')) : deeds = [];
 
-class aim = {
+class aim {
     constructor(description) {
         this.description = description;
         this.isCompleted = false;
     }
 }
 
-deeds.forEach((element) => {
-    wrapper.innerHTML += `<form><div class="deeds">${element.description}<button class="done">✓</button><button class="delete">X</button></div></form>`;
-});
 
-let update = () => {
+let createDiv = (deed, index) => {
+return `<form><div class="deeds">${deed.description}<input type="checkbox" onchange="done(${index})" class="done"></input><button onclick="deleteDeed(${index})" class="delete">X</button></div></form>`;
+};
+
+
+let fillList = () => {
+    toDoList.innerHTML = "";
+    if (deeds.length > 0) {
+        deeds.forEach((item, index) => {
+            toDoList.innerHTML += createDiv(item, index);
+        })
+    deedsItems = document.querySelectorAll(".deeds");
+    deeds.forEach((item, index) => {if (item.isCompleted) {
+        deedsItems[index].classList.add('isdone');
+    } else {
+        deedsItems[index].classList.remove('isdone');
+    }
+})
+    }
+};
+
+fillList();
+
+let addToStorage = () => {
     localStorage.setItem('deeds', JSON.stringify(deeds));
 };
 
+done = (index) => {
+    deeds[index].isCompleted = !deeds[index].isCompleted;
+    addToStorage();
+    fillList();
+}
+
+deleteDeed = (index) => {
+    deeds.splice(index, 1);
+    addToStorage();
+    fillList();
+}
+
+
 btn.addEventListener("click", () => {
-let message = action.value;
-let task = new aim(message);
-deeds.push(task);
-update();
+deeds.push(new aim(action.value));
+addToStorage();
 })
