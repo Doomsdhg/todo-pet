1f58728e7a04254b173fec3f77ed7d68383427a9
Антон <tohanknv@yandex.ru>
2021-08-11 09:41:58 +0300

    +sorting

--- script.js
+++ script.js
@@ -17,13 +17,24 @@ class aim {
 
 
 let createDiv = (deed, index) => {
-return `<form><div class="deeds">${deed.description}<input type="checkbox" onchange="done(${index})" class="done"></input><button onclick="deleteDeed(${index})" class="delete">X</button></div></form>`;
+return `<div class="deeds">
+<div class="description">${deed.description}</div>
+<div class="buttons">
+<label class="custom-checkbox">
+<input type="checkbox" id="checkbox${index}" onclick="done(${index})">
+<span></span>
+</label>
+<button onclick="deleteDeed(${index})" class="delete">X</button>
+</div>
+</div>`;
 };
+
 
 
 let fillList = () => {
     toDoList.innerHTML = "";
     if (deeds.length > 0) {
+        sort();
         deeds.forEach((item, index) => {
             toDoList.innerHTML += createDiv(item, index);
         })
@@ -33,20 +44,43 @@ let fillList = () => {
     } else {
         deedsItems[index].classList.remove('isdone');
     }
+    
 })
     }
 };
 
+let sort = () => {
+    let allDeeds = deeds.length && deeds.filter(task => task.isCompleted == false);
+    let doneDeeds = deeds.length && deeds.filter(task => task.isCompleted == true);
+    deeds = [...allDeeds, ...doneDeeds];
+}
+
+/*let sort = () => {
+    deeds.forEach((item, index) => {
+        deedsItems = document.querySelectorAll(".deeds");
+        if (deedsItems[index].classList.contains("isdone")) {
+            let temp = deeds[index];
+            deeds.push(temp);                
+            deeds.splice(index, 1);
+        }
+    }
+    )
+}
+sort();*/
 fillList();
 
 let addToStorage = () => {
     localStorage.setItem('deeds', JSON.stringify(deeds));
 };
 
-done = (index) => {
+let done = (index) => {
+    let chckbx = document.getElementById(`checkbox${index}`);
+    if (chckbx.checked) {
     deeds[index].isCompleted = !deeds[index].isCompleted;
+    }
     addToStorage();
-    fillList();
+    fillList();    
+    
 }
 
 deleteDeed = (index) => {
